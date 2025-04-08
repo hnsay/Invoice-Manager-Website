500fda54d3a0658aec0f51cdc87b964a5bac3e08
Halil Say <github@hnsay.com.tr>
2025-04-08 19:09:22 +0300

    moving config file

--- process_bulk.php
+++ process_bulk.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
       exit;
 }
 
-require_once "config.php";
+require_once "/config/config.php";
 require_once "error_log.php"; 
 
 mysqli_close($link);
