8de34ff8cbc26b03985b074b2999c70363317818
Halil Say <github@hnsay.com.tr>
2025-04-13 23:40:04 +0300

    some definitions added to config.php

--- config/config.php
+++ config/config.php
@@ -17,6 +17,14 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'test');
 define('DB_NAME', 'invoice_tracker_test');
+define('LOGIN_URL', '/app/views/login.php');
+define('LOGOUT_URL', '/hepers/logout.php');
+define('CALL_URL', '/helpers/call.php');
+define('CALL_PENDING_URL', '/helpers/call_pending.php');
+define('CALL_PENDING_FINANCE_URL', '/helpers/call_pending_finance.php');
+define('TABLE_MANAGE_URL', '/helpers/table_manage.php.php');
+define('TABLE_PROCESS_URL', '/helpers/table_process.php');
+define('TABLE_PROCESS_LITE_URL', '/helpers/table_process_lite.php');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
