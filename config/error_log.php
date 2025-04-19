2ea23f9c4ef6679755ab7808bf8b1ccf33f1d7ca
Halil Say <github@hnsay.com.tr>
2025-04-19 23:18:40 +0300

    don't display errors

--- config/error_log.php
+++ config/error_log.php
@@ -13,7 +13,7 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 ini_set('display_errors', 1); 
-ini_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 ini_set("log_errors", 1);
 ini_set("error_log", $_SERVER['DOCUMENT_ROOT'] . "/logs/php-error.log");
